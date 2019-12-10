00a579b2d5f654e7b7ce6cdfe11e6074d24f0e11
Yining Wang <yining.wang@utexas.edu>
2019-12-10 17:56:46 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    let element = document.querySelector("p#text");
+    element.innerHTML = "This is really cool!";
+});
+
+// element = document.querySelector("p#text");
+// element.innerHTML = "This is really cool!";
